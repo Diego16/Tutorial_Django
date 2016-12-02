1e6383a32fd5ab11bfbc9066b2c74a8bff03a85e
Diego16 <diegogb0113@gmail.com>
2016-12-02 14:09:46 -0500

    Modifications up to tutorial #5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from .models import Question, Choice
 from django.views import generic
 # Create your views here.
@@ -8,31 +9,23 @@ class IndexView(generic.ListView):
 	template_name ='polls/index.html'
 	context_object_name ='latest_question_list'
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
 	model = Question
 	template_name= 'polls/detail.html'
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 class ResultsView(generic.DetailView):
 	model= Question
 	template_name='polls/results.html'
-"""def detail(request, question_id):
-	try:
-		question=get_object_or_404(Question, pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
-	return render(request, 'polls/detail.html',{'question':question})
-def results(request,question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html',{'question':question})"""
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
 def vote(request,question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except(KeyError, Choice.DoesNotExist):
-		return render(request,'polls/detail.html',{
-			'question':question,
-			'error_message':"You didn't select a choice."
-			})
+		return render(request,'polls/detail.html',{'question':question,'error_message':"You didn't select a choice."})
 	else:
 		selected_choice.votes +=1
 		selected_choice.save()
